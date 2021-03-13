eb2ba87638d3d0a2ca7caa4cbbfcd583a3afc3e0
JoaoVictorAragao <johnny.aragao@gmail.com>
2021-03-13 15:29:52 -0300

    Segundo teste

--- src/main/java/com/project/professor/alocation/ProfessorAlocationApplication.java
+++ src/main/java/com/project/professor/alocation/ProfessorAlocationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Primeiro Teste
+//Segundo Teste
 
 @SpringBootApplication
 public class ProfessorAlocationApplication {
