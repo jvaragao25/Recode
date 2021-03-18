b31ede96320cf4d4a0e965fb8b10e83dd608086b
JoaoVictorAragao <johnny.aragao@gmail.com>
2021-03-18 14:44:08 -0300

    Homework 5.1

--- src/main/java/com/project/professor/alocation/repository/AlocacaoRepositorio.java
+++ src/main/java/com/project/professor/alocation/repository/AlocacaoRepositorio.java
@@ -1,12 +1,19 @@
 package com.project.professor.alocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.project.professor.alocation.entity.Alocacao;
+import com.project.professor.alocation.entity.Curso;
+import com.project.professor.alocation.entity.Professor;
 
 @Repository
 
 public interface AlocacaoRepositorio extends JpaRepository<Alocacao, Long>{
+	
+	List<Alocacao> findByProfessor (Long id);
+	List<Alocacao> findByCurso (Long id);
 
 }
--- src/main/java/com/project/professor/alocation/repository/ProfessorRepositorio.java
+++ src/main/java/com/project/professor/alocation/repository/ProfessorRepositorio.java
@@ -1,5 +1,7 @@
 package com.project.professor.alocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.project.professor.alocation.entity.Professor;
 @Repository
 
 public interface ProfessorRepositorio extends JpaRepository<Professor, Long>{
+	
+	List<Professor> findByNameContainingIgnoreCase (String name);
 
 }
